d32c65aa0a4806d5ffe8d5e780c6661624c1ccf3
wisin007castro <castrow666@gmail.com>
2017-08-21 16:53:24 -0400

    Agregando el Proyecto

---
+++ Geodesica/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGradosLong = new System.Windows.Forms.TextBox();
+            this.txtMinLong = new System.Windows.Forms.TextBox();
+            this.txtSegLong = new System.Windows.Forms.TextBox();
+            this.txtSegLat = new System.Windows.Forms.TextBox();
+            this.txtMinLat = new System.Windows.Forms.TextBox();
+            this.txtGradosLat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cbLong = new System.Windows.Forms.ComboBox();
+            this.cbLat = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(117, 273);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(82, 23);
+            this.btnConvertir.TabIndex = 0;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Longitud";
+            // 
+            // txtGradosLong
+            // 
+            this.txtGradosLong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGradosLong.Location = new System.Drawing.Point(99, 110);
+            this.txtGradosLong.Name = "txtGradosLong";
+            this.txtGradosLong.Size = new System.Drawing.Size(35, 22);
+            this.txtGradosLong.TabIndex = 2;
+            // 
+            // txtMinLong
+            // 
+            this.txtMinLong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinLong.Location = new System.Drawing.Point(155, 110);
+            this.txtMinLong.Name = "txtMinLong";
+            this.txtMinLong.Size = new System.Drawing.Size(49, 22);
+            this.txtMinLong.TabIndex = 3;
+            // 
+            // txtSegLong
+            // 
+            this.txtSegLong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSegLong.Location = new System.Drawing.Point(223, 110);
+            this.txtSegLong.Name = "txtSegLong";
+            this.txtSegLong.Size = new System.Drawing.Size(63, 22);
+            this.txtSegLong.TabIndex = 4;
+            // 
+            // txtSegLat
+            // 
+            this.txtSegLat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSegLat.Location = new System.Drawing.Point(223, 145);
+            this.txtSegLat.Name = "txtSegLat";
+            this.txtSegLat.Size = new System.Drawing.Size(63, 22);
+            this.txtSegLat.TabIndex = 8;
+            // 
+            // txtMinLat
+            // 
+            this.txtMinLat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinLat.Location = new System.Drawing.Point(155, 145);
+            this.txtMinLat.Name = "txtMinLat";
+            this.txtMinLat.Size = new System.Drawing.Size(49, 22);
+            this.txtMinLat.TabIndex = 7;
+            // 
+            // txtGradosLat
+            // 
+            this.txtGradosLat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGradosLat.Location = new System.Drawing.Point(99, 145);
+            this.txtGradosLat.Name = "txtGradosLat";
+            this.txtGradosLat.Size = new System.Drawing.Size(35, 22);
+            this.txtGradosLat.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Latitud";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(134, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "°";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(204, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "\'";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(289, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "\"";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(289, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "\"";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(204, 145);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "\'";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(134, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "°";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(99, 247);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 248);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Resultado";
+            // 
+            // cbLong
+            // 
+            this.cbLong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLong.FormattingEnabled = true;
+            this.cbLong.Items.AddRange(new object[] {
+            "Este",
+            "Oeste"});
+            this.cbLong.Location = new System.Drawing.Point(325, 110);
+            this.cbLong.Name = "cbLong";
+            this.cbLong.Size = new System.Drawing.Size(72, 24);
+            this.cbLong.TabIndex = 17;
+            this.cbLong.Text = "Oeste";
+            // 
+            // cbLat
+            // 
+            this.cbLat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLat.FormattingEnabled = true;
+            this.cbLat.Items.AddRange(new object[] {
+            "Norte",
+            "Sur"});
+            this.cbLat.Location = new System.Drawing.Point(325, 147);
+            this.cbLat.Name = "cbLat";
+            this.cbLat.Size = new System.Drawing.Size(72, 24);
+            this.cbLat.TabIndex = 18;
+            this.cbLat.Text = "Norte";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Azure;
+            this.label10.Location = new System.Drawing.Point(12, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(204, 21);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Coordenadas Geográficas";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Azure;
+            this.label11.Location = new System.Drawing.Point(11, 204);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(150, 21);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Coordenadas UTM";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Black;
+            this.label12.Location = new System.Drawing.Point(12, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(388, 32);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Coordenadas Geográficas a UTM";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(225, 206);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 221);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Unrealised", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label13.Location = new System.Drawing.Point(10, 393);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(158, 34);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "William Castro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(472, 439);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cbLat);
+            this.Controls.Add(this.cbLong);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSegLat);
+            this.Controls.Add(this.txtMinLat);
+            this.Controls.Add(this.txtGradosLat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSegLong);
+            this.Controls.Add(this.txtMinLong);
+            this.Controls.Add(this.txtGradosLong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvertir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGradosLong;
+        private System.Windows.Forms.TextBox txtMinLong;
+        private System.Windows.Forms.TextBox txtSegLong;
+        private System.Windows.Forms.TextBox txtSegLat;
+        private System.Windows.Forms.TextBox txtMinLat;
+        private System.Windows.Forms.TextBox txtGradosLat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cbLong;
+        private System.Windows.Forms.ComboBox cbLat;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Geodesica/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            // elipsoide de Hayford
+            int a = 6378388; //semieje mayor
+            double b = 6356911.94613; //semieje menor
+
+
+            //definiendo variables
+            double c, e1, e2, e22, GradLong, GradLat, RadLong, RadLat, huso, husoMer;
+            double delamb, A, xi, eta, ni, zeta, A1, A2, j2, j4, j6;
+            double alfa, beta, gamma, resultado;
+
+
+
+            e1 = Math.Sqrt(Math.Pow(a, 2) - Math.Pow(b, 2)) / a; //Srimera excentricidad
+            e2 = Math.Sqrt(Math.Pow(a, 2) - Math.Pow(b, 2)) / b; //Segunda excentricidad
+            e22 = Math.Pow(e2, 2);
+
+
+            c = Math.Pow(a, 2) / b;
+
+            try
+            {
+                //convirtiendo la Longitud y Latitud de grados sexagesimales a grados Decimales
+                GradLong = Convert.ToDouble(txtGradosLong.Text) + ((Convert.ToDouble(txtMinLong.Text)) / 60) + ((Convert.ToDouble(txtSegLong.Text)) / 60) / 60;
+                GradLat = Convert.ToDouble(txtGradosLat.Text) + ((Convert.ToDouble(txtMinLat.Text)) / 60) + ((Convert.ToDouble(txtSegLat.Text)) / 60) / 60;
+
+                if (cbLong.Text.Equals("Oeste"))
+                {
+                    GradLong = GradLong * (-1); //en caso de que la Longitud sea de Oeste multiplicar por (-1) 
+                }
+                if (cbLat.Text.Equals("Sur")) 
+                {
+                    GradLat = GradLat * (-1);//en caso de que la Longitud sea de Sur multiplicar por (-1)
+                }
+
+                //convirtiendo la Longitud y Latitud de Decimales a Radianes
+                RadLong = GradLong * Math.PI / 180;
+                RadLat = GradLat * Math.PI / 180;
+
+                //Calculando el Huso
+                huso = Math.Truncate((GradLong / 6) + 31);
+                husoMer = huso * 6 - 183;
+
+                delamb = RadLong - ((husoMer * Math.PI) / 180);
+
+                A = Math.Cos(RadLat) * Math.Sin(delamb);
+
+                xi = 0.5 * Math.Log((1 + A) / (1 - A));
+
+                eta = Math.Atan(Math.Tan(RadLat) / Math.Cos(delamb)) - RadLat;
+
+                ni = (c / Math.Pow((1 + e22 * Math.Pow(Math.Cos(RadLat), 2)), (0.5))) * 0.9996;
+
+                zeta = (e22 / 2) * Math.Pow(xi, 2) * Math.Pow(Math.Cos(RadLat), 2);
+
+                A1 = Math.Sin(RadLat * 2);
+
+                A2 = A1 * Math.Pow(Math.Cos(RadLat), 2);
+
+                j2 = RadLat + (A1 / 2);
+
+                j4 = ((3 * j2) + A2) / 4;
+
+                j6 = (5 * j4 + A2 * (Math.Pow(Math.Cos(RadLat), 2))) / 3;
+
+                alfa = (3 / 4) * e22;
+                beta = (5 / 3) * Math.Pow(alfa, 2);
+                gamma = (35 / 27) * Math.Pow(alfa, 3);
+
+
+
+                resultado = xi * ni * (1 + zeta / 3) + 500000;
+
+
+                txtResultado.Text = resultado.ToString();
+                        
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Verifique que los datos esten correctos", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
